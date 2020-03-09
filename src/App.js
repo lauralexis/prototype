c0df54cddd5273d3f7ca8f7c96aa0ff9d939ddbb
Laura Pavlov <lauraweiner@boomi.com>
2020-03-09 10:16:46 -0400

    Removes boilerplate styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Prototyping Environment, Yay!
-        </p>
-      </header>
-    </div>
+    <header>
+      <img src={logo} alt="logo" />
+      <p>
+        Welcome to our prototyping environment!
+      </p>
+    </header>
   );
 }
 
