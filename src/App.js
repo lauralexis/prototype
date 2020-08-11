ef4f2e7f746d0f8343a50b3a08c7adfdf311bc35
Laura Pavlov <lauraweiner@boomi.com>
2020-08-11 11:01:34 -0400

    Add sample html to create theme

--- src/App.js
+++ src/App.js
@@ -4,9 +4,149 @@ import {ReactComponent as CaretSVG} from './icons/caret.svg';
 
 function App() {
   return (
-    <header>
-      <h1> Welcome to our prototyping environment! </h1>
-    </header>
+    <div id="manywho">
+      <div class="mw-bs flow-container">
+        <div class="main-container">
+          <div class="main-container-inner">
+            <nav class="navbar navbar-default">
+              <div class=" container">
+                <div class="navbar-header">
+                  <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#f9c018f7-5a59-4858-924a-e958c154dae4">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+              <div class="collapse navbar-collapse" id="f9c018f7-5a59-4858-924a-e958c154dae4">
+              <ul class="nav navbar-nav">
+              <li class="active top-nav-element"><a href="#" id="a9b17b96-ebd7-d49a-f04d-9ed2f88febc4">Home</a></li>
+              <li class="top-nav-element"><a href="#" id="d996a044-dff3-8202-0e9d-046eae619e80">Payment Details</a></li>
+              <li class="top-nav-element"><a href="#" id="aec9a9aa-0d30-a176-3cfe-24beabdd553c">Current Cycle Usage</a></li>
+              <li class="top-nav-element"><a href="#" id="5707634b-598b-8216-c75d-6e0015244578">Billing History</a></li>
+              <li class="top-nav-element"><a href="#" id="779e9597-80b0-7e7c-279d-7d9145ecee12">Deactivate</a></li>
+              </ul>
+              </div>
+              </div>
+            </nav>
+            <div class="main-scroller">
+              <div class="main container">
+                <h2 class="page-label"></h2>
+                <div class="mw-horizontal_flow capp-body admin-dash clearfix mw-container">
+                  <h3>Account Details</h3>
+                  <div class="row clearfix" id="horizontal">
+                    <div class="mw-presentation col-sm-12">
+                      <div><label></label>
+                        <div>
+                          <h4>Account Name: <strong>Elastic Usage Sample Inc.</strong></h4>
+                          <h4>Contact Email: <strong>chriscappetta+elasticusage84@boomi.com</strong></h4>
+                          <h4>Atom Account ID: <strong>boomi_andytillo-FU39SW</strong></h4>
+                          <h4>Flow Tenant ID: <strong>55e9b01a-fc9e-4c66-aaa2-9360aea9eaaf</strong></h4>
+                          <h4>Customer ID: <strong>cus_Hm2bSSdxGTwWiO</strong></h4>
+                          <p><em>Logged in as: </em></p>
+                          <p><em>Input data: </em></p>
+                        </div>
+                      <span class="help-block"></span>
+                      <span class="help-block"></span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button class="outcome btn btn-primary" title="View Current Cycle Usage">View Current Cycle Usage</button>
+                <noscript></noscript>
+              </div>
+            </div>
+
+            <div class="main-scroller">
+              <div class="main container">
+                <h2 class="page-label"></h2>
+                  <div class="mw-vertical_flow capp-body clearfix mw-container">
+                    <div class="clearfix" id="vertical">
+                    <div class="mw-presentation">
+                      <div><label></label>
+                        <div>
+                          <h3>You have used <strong><em>2,167,824</em></strong> messages this billing period.</h3>
+                        </div>
+                        <span class="help-block"></span>
+                        <span class="help-block"></span>
+                      </div>
+                    </div>
+                  <div class="mw-presentation hidden">
+                    <div><label></label>
+                      <div>
+                        <p>Looks like you don't have much usage for this billing period.</p>
+                        <p><a href="https://boomi.to/elastic_user2" target="_blank" rel="noopener">Click here to do that!</a></p>
+                      </div>
+                      <span class="help-block"></span>
+                      <span class="help-block"></span>
+                  </div>
+                </div>
+                  <div class="mw-vertical_flow clearfix mw-container">
+                  <h3><span class="glyphicon glyphicon-minus"></span>Message Details</h3>
+                    <div class="ReactCollapse--collapse">
+                      <div class="ReactCollapse--content">
+                        <div class="clearfix" id="vertical">
+                          <div class="table-container clear-fix mw-table">
+                            <div class="clearfix ">
+                              <div class="mw-items-header">
+                                <div class="mw-items-header-outcomes"></div>
+                              </div>
+                              <div class="table-responsive">
+                                <table class="table">
+                                  <tbody>
+                                    <tr>
+                                      <th data-sort-property="item_identifier">Item</th>
+                                      <th data-sort-property="stripe_subscription_id">ID</th>
+                                      <th data-sort-property="subscription_type">Billing Format</th>
+                                      <th data-sort-property="usage_count">Messages this period</th>
+                                      <th data-sort-property="item_current_usage_cost">Cost</th>
+                                      <th data-sort-property="period_start">Period Start</th>
+                                      <th data-sort-property="period_end">Period End</th>
+                                    </tr>
+                                    <tr>
+                                      <td><span>Integration/Hub/B2B Messages</span></td>
+                                      <td><span>si_Hm2b8PQZlbMpT1</span></td>
+                                      <td><span>Period usage</span></td>
+                                      <td><span>2,047,842</span></td>
+                                      <td><span>$9,349.10</span></td>
+                                      <td><span>08/04/2020</span></td>
+                                      <td><span>09/04/2020</span></td>
+                                    </tr>
+                                    <tr>
+                                      <td><span>Flow Messages</span></td>
+                                      <td><span>si_Hm2b6uX8Fildli</span></td>
+                                      <td><span>Period usage</span></td>
+                                      <td><span>116,400</span></td>
+                                      <td><span>$1,765.77</span></td>
+                                      <td><span>08/04/2020</span></td>
+                                      <td><span>09/04/2020</span></td>
+                                    </tr>
+                                    <tr>
+                                      <td><span>APIM Messages</span></td>
+                                      <td><span>si_Hm2bmg9eGCfN8I</span></td>
+                                      <td><span>Period usage</span></td>
+                                      <td><span>3,582</span></td>
+                                      <td><span>$187.52</span></td>
+                                      <td><span>08/04/2020</span></td>
+                                      <td><span>09/04/2020</span></td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
